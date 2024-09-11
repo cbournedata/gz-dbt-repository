6ced9afac7d1f3c48cea35a0b600f9d04086eb8b
cbournedata@gmail.com <cbournedata@gmail.com>
2024-09-11 11:37:55 +0000

    created int_orders_margin to group by orders and date, and added some cleaning functions to both intermediate models (orders and sales margin).

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,3 +1,6 @@
-SELECT *
-FROM {{ ref('int_sales_margin') }} AS sales_margin
+SELECT orders_id, date_date, ROUND(SUM(revenue),2) AS revenue, SUM(quantity) AS quantity, ROUND(SUM(purchase_cost),2) AS purchase_cost, ROUND(SUM(margin),2) AS margin
+FROM {{ ref('int_sales_margin') }}
+GROUP BY orders_id, date_date
+ORDER BY orders_id DESC
+
 
--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,5 +1,10 @@
+WITH purchase_cost_table AS (
+    SELECT sales.*, product.purchase_price, ROUND(sales.quantity * product.purchase_price,2) AS purchase_cost
+    FROM {{ ref('stg_raw__sales')}} AS sales
+    LEFT JOIN {{ ref('stg_raw__product')}} AS product
+    ON sales.products_id = product.products_id
+)
 
-SELECT sales.*, product.purchase_price, sales.quantity * product.purchase_price AS purchase_cost, sales.revenue - product.purchase_price AS margin
-FROM {{ ref('stg_raw__sales')}} AS sales
-LEFT JOIN {{ ref('stg_raw__product')}} AS product
-ON sales.products_id = product.products_id
+
+SELECT *, ROUND(revenue - purchase_cost,2) AS margin
+FROM purchase_cost_table
