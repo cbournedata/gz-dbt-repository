47160e0ecc8381e644931ac02079e20965645301
cbournedata@gmail.com <cbournedata@gmail.com>
2024-09-11 11:01:39 +0000

    created int_sales_margin model with calculated columns purchase_cost and margin

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,5 @@
+
+SELECT sales.*, product.purchase_price, sales.quantity * product.purchase_price AS purchase_cost, sales.revenue - product.purchase_price AS margin
+FROM {{ ref('stg_raw__sales')}} AS sales
+LEFT JOIN {{ ref('stg_raw__product')}} AS product
+ON sales.products_id = product.products_id
