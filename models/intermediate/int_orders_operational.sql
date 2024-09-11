31fbce508ca9fa2f0d3410b8c7829a1ecf9e1703
cbournedata@gmail.com <cbournedata@gmail.com>
2024-09-11 14:22:45 +0000

    initilised finance_days mart model and modified int_orders_operational to include columns needed for aggregation in the finance_days model

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -4,7 +4,10 @@ SELECT ord_marg.orders_id, ord_marg.date_date, ROUND(SUM(
       CAST(ship.shipping_fee AS FLOAT64) - 
       CAST(ship.logCost AS FLOAT64) - 
       CAST(ship.ship_cost AS FLOAT64)
-    ),2) AS operational_margin
+    ),2) AS operational_margin,
+    ROUND(SUM(ship.shipping_fee),2) AS shipping_fee,
+    ROUND(SUM(ship.logCost),2) AS log_cost,
+    ROUND(SUM(revenue),2) AS revenue, SUM(quantity) AS quantity, ROUND(SUM(purchase_cost),2) AS purchase_cost, ROUND(SUM(margin),2) AS margin
 FROM {{ ref('int_orders_margin') }} AS ord_marg
 LEFT JOIN {{ ref('stg_raw__ship')}} AS ship
 ON ord_marg.orders_id = ship.orders_id
