e25e414c59a1ec9eda38186227ffded129d8eb9b
cbournedata@gmail.com <cbournedata@gmail.com>
2024-09-11 13:34:27 +0000

    created int_orders_operational model for operational margin per order

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,13 @@
+
+SELECT ord_marg.orders_id, ord_marg.date_date, ROUND(SUM(
+      CAST(ord_marg.margin AS FLOAT64) + 
+      CAST(ship.shipping_fee AS FLOAT64) - 
+      CAST(ship.logCost AS FLOAT64) - 
+      CAST(ship.ship_cost AS FLOAT64)
+    ),2) AS operational_margin
+FROM {{ ref('int_orders_margin') }} AS ord_marg
+LEFT JOIN {{ ref('stg_raw__ship')}} AS ship
+ON ord_marg.orders_id = ship.orders_id
+GROUP BY orders_id, date_date
+ORDER BY orders_id DESC
+
